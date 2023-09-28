afe2f65039bbea5adb0afc1a1daa7dd4948f8f1b
joest3r <93015321+joest3r@users.noreply.github.com>
2023-09-28 20:32:49 -0300

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,12 @@
+alert("Arraste el mouse sobre la botonera azul")
+function mostrar(caja){
+var boton = document.getElementById(caja)
+boton.style.display='block'
+
+}
+function ocultar(caja){
+
+    var boton = document.getElementById(caja)
+    boton.style.display='none'
+
+}
